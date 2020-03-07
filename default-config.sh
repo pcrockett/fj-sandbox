a623871fd61d690a3663abd31101824f6ebac5e9
Philip Crockett <contact@philcrockett.com>
2020-03-07 10:11:58 +0100

    Simple config mechanism

---
+++ default-config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# To override any of these defaults, create a new script in this directory
+# called "sandbox-config.sh" and re-export the environment variables you want
+# to override.
+
+export FJSB_SCREEN_SIZE="800x600"
+export FJSB_SANDBOX_ROOT=~/sandboxes
